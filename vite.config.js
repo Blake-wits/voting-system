b96d618f1882ca7f195ed361864f2eadb0e02c93
Blake <blakehung@wits.com>
2024-09-04 23:02:42 +0800

    chore: edit env's setting

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
-      external: ['axios']
-    }
-  }
+      output: {
+        manualChunks: {
+          'pdfjs': ['pdfjs-dist'],
+          'pdf-lib': ['pdf-lib'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // 增加塊大小警告限制
+  },
+  optimizeDeps: {
+    include: ['pdfjs-dist', 'pdf-lib']
+  },
+  server: {
+    port: 3000, // 或者您想要的任何端口
+  },
+  preview: {
+    port: 4173, // Vite 預覽的默認端口
+  },
 })
